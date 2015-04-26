19fb7fcabcf15e5b2838207ba22e24f3734f5f2b
Marshall Farrier <info@marshallfarrier.com>
2015-04-25 22:58:20 -0700

    Logging

--- lib/service.py
+++ lib/service.py
@@ -10,4 +10,12 @@ def equities():
     return _ret;
 
 if __name__ == '__main__':
+    import logging
+    logger = logging.getLogger('eqpuller')
+    handler = logging.FileHandler('/mnt/disk1/var/log/optbot/eqpuller.log')
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s.%(funcName)s :  %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.info(equities())
     print(equities())
