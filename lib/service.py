41aefdacddd06c43287aee4b85a00308bdb4ad4a
Marshall Farrier <info@marshallfarrier.com>
2015-04-26 01:16:15 -0700

    Attempt to run child process as eqpuller user.

--- lib/service.py
+++ lib/service.py
@@ -12,10 +12,9 @@ def equities():
 if __name__ == '__main__':
     import logging
     logger = logging.getLogger('eqpuller')
-    handler = logging.FileHandler('/mnt/disk1/var/log/optbot/eqpuller.log')
+    handler = logging.FileHandler('/mnt/disk1/var/log/optbot/eqpuller/service.log')
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s.%(funcName)s :  %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     logger.info(equities())
-    print(equities())
