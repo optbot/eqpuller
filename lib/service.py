b48d4ed6106c8e483dbff02278d1b3a01a865dde
Marshall Farrier <info@marshallfarrier.com>
2015-04-25 17:30:17 -0700

    Run python script, remove branch dependency. Fixes #1

---
+++ lib/service.py
@@ -0,0 +1,13 @@
+def equities():
+    _ret = [
+            'ddd',
+            'ge',
+            'nflx',
+            'scty',
+            'spwr',
+            'tsla',
+            ]
+    return _ret;
+
+if __name__ == '__main__':
+    print(equities())
